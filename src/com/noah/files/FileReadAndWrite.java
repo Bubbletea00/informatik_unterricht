de68ca64a59df45447bbc37be0634e1054ffb49f
Noah Heckel <145144013+Bubbletea00@users.noreply.github.com>
2025-11-30 21:31:46 +0100

    Never gonna run around and desert you

--- src/com/noah/files/FileReadAndWrite.java
+++ src/com/noah/files/FileReadAndWrite.java
@@ -2,60 +2,24 @@ package com.noah.files;
 
 import java.io.*;
 
+
+//Aufgabe 1: Füge unter try{} Code hinzu, der einen Fehler verursacht.
+
+//Aufgabe 2: Erstelle die Methode printShrekWhatCount() und gib die summe an Zeilen aus,
+//           in welchen ein "what?" vorkommt.
+
+//Aufgabe 3* (für schnelle): Erstelle einen Konstruktor, getter und setter für die Klasse,
+//                           damit du sie sinnvoll für dich nutzen kannst.
+
+
 public class FileReadAndWrite {
-    public static void main(String[] args) {
-        FileReadAndWrite f = new FileReadAndWrite();
-        f.tryCatch();
-
-
-        if (f.file.exists()) {
-            System.out.println("Datei existiert bereits");
-        } else {
-            System.out.println("Datei wird erstellt");
-        }
-
-        f.writeUsingBufferedWriter();
-        System.out.println(f.readUsingBufferedReader());
-
-
-    }
-
-
     private String path = "res/test.txt";
-    private String content = "Hello World\r\nabc\r\nd";
+    private String content = "Hello World\r\nLorem\r\nIpsum";
 
     private File file = new File(path);
 
-    public void writeUsingBufferedWriter() {
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))){
-            writer.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.err.println("error with the BufferedWriter");
-        }
-    }
-
-
-    public String readUsingBufferedReader(){
-        StringBuilder sb = new StringBuilder();
-
-        // try-with-resources
-        try (BufferedReader b = new BufferedReader(new FileReader(path))) {
-            String line;
-            while ((line = b.readLine()) != null) {
-                System.out.println(line);
-                sb.append(line).append(System.lineSeparator());
-            }
-
-
-        }catch (IOException e){
-            System.err.println("error with the BufferedReader");
-        }
-        return sb.toString();
-    }
-
-    public void tryCatch(){
+    public void tryCatch() {
         try {
             //Füge hier deinen Code ein
         } catch (Exception e) {
@@ -65,5 +29,60 @@ public class FileReadAndWrite {
         }
     }
 
+    public void printFileStatus() {
+        if (file.exists()) {
+            System.out.println("File exists");
+        } else {
+            System.out.println("File does not exist");
+        }
+    }
 
+    public void writeUsingBufferedWriter() {
+
+        // try-with-resources
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
+            writer.write(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("error with the BufferedWriter");
+        }
+    }
+
+    public void readUsingBufferedReader() {
+        StringBuilder sb = new StringBuilder();
+
+        // try-with-resources
+        try (BufferedReader b = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = b.readLine()) != null) {
+                sb.append(line).append(System.lineSeparator());
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("error with the BufferedReader");
+        }
+
+        System.out.println(sb.toString());
+    }
+
+    /**
+     * Diese Methode soll die Anzahl der Zeilen ausgeben,
+     * in welchen ein "What?" im Script von Shrek vorkommt.
+     */
+    public void printShrekWhatCount() {
+        String shrekScriptPath = "res/shrek_script.txt";
+        int count = 0;
+
+
+        System.out.println("Es gibt " + count + " Zeilen, in denen What? vorkommt.");
+    }
+
+    /**
+     * Diese Methode gibt 'true' zurück, wenn in der Line der searchString vorkommt.
+     */
+    private boolean searchLine(String searchString, String line) {
+        return line.contains(searchString);
+    }
 }
